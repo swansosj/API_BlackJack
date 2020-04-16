f840be0696e0549bb44242a7b1fb8fe9441327a0
swansosj <sheldon.j.swanson@gmail.com>
2020-04-16 16:42:37 -0400

    My first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+
+This is an ongoing project to get more familiar with Python and APIs
+with Python.
+
+"""
+#Import the request library to make the API call
+import requests
+
+#Prompt the user for how many decks they want
+
+
+#Creates empty dictionaries for the first request
+payload = {}
+headers = {'Accept': 'application/json'}
+NewShuff = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+
+response = requests.get(NewShuff, headers=headers, data=payload)
+
+responseJSON = response.json()
+DeckID = responseJSON['deck_id']
+
+print(responseJSON)
+print("Your Deck id is: " + DeckID)
+
+carddata = {}
+
+DrawCard = "https://deckofcardsapi.com/api/deck/" + DeckID + "/draw/?count=1"
+
+card = requests.get(DrawCard, headers=headers, data=carddata)
+
+cardInfo = card.json()['cards'][0]
+
+CardSuit = cardInfo['suit']
+CardValue = cardInfo['value']
+
+
+print(CardValue + " of " + CardSuit)
